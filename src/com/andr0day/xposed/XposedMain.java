24cdffde8cdeb2e72ffaa1176ad2ae8dbaccccd0
andr0day <andr0day@Macintosh.local>
2015-05-23 20:42:56 +0800

    add xposed

---
+++ src/com/andr0day/xposed/XposedMain.java
@@ -0,0 +1,11 @@
+package com.andr0day.xposed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedMain implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+}
